97b59c8cdb835a28e186c3f6ad5a486c7be51f5c
Rushikesh Barwade <rushikeshbarwade@gmail.com>
2026-04-23 14:23:12 +0000

    Comparisons

---
+++ 01_basics/Comparisons.js
@@ -0,0 +1,26 @@
+// console.log(1>2);
+// console.log(1>=2);
+// console.log(1==2);
+// console.log(1<2);
+// console.log(1<=2);
+
+//make sure while comparing you are comparing same data types
+
+// Equality checks (==) and comparisons work differently 
+// bcoz comparisons convert null to 0 and then compares ex.
+
+// console.log(null > 0); //o/p is false
+// console.log(null == 0); // false
+// console.log(null>=0); // true
+
+// console.log(undefined > 0); //o/p is false
+// console.log(undefined == 0); // false
+// console.log(undefined>=0); // false
+
+// === > strict check
+
+// a = ("2" === 2) // avoid comparing different data types
+// console.log(a); // it checks the data type while strictly comparing/checking 2 values
+// console.log(typeof a); // type is boolean
+
+                        // 
---
+++ 01_basics/DataTypes&ECMAStandards.js
@@ -0,0 +1,19 @@
+"use strict"; // whatever code we write will be considered as newer version
+
+// alert(3+3) // this is available in browser console, but written in
+           // different format or syntax in JS (node supported)
+
+// Data Types
+// Primitive Data Types
+// string => text/text+number+anything within " "
+// number = > number, 2 to power 53
+//bigint => bigger number than number eg/ stock market etc.
+// boolean => true/false
+// null => standalone value; it's an object
+
+console.log(typeof null) //DT is object
+// undefined => let state; (no state defined); DT is undefined
+
+console.log(typeof undefined)
+
+// object =>
