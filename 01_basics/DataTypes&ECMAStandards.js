f9777ce5fdbb40e34cc0e93a48a244042b4cc7c3
Rushikesh Barwade <rushikeshbarwade@gmail.com>
2026-04-23 15:07:34 +0000

    latest

--- 01_basics/DataTypes&ECMAStandards.js
+++ 01_basics/DataTypes&ECMAStandards.js
@@ -17,3 +17,29 @@ console.log(typeof null) //DT is object
 console.log(typeof undefined)
 
 // object =>
+
+
+// Non primitive data types (Reference DTs) : Array, Objects, Functions
+
+// const arrayValue = ["Rushikesh", 234, "Barwade"] // Array
+
+// const objectValue = {            // Objects
+
+//     name: "Rushikesh",
+//     age: 23
+
+// }
+
+// const functionValue = function(){     // Functions
+
+//     a: "Rushikesh",
+//     number = 1224567
+// }
+
+let id = Symbol('123')
+let customerId = Symbol('123')
+
+console.log(id === customerId);
+
+// DT of Array is Object, of Object is Object and of Function of Object Function/Function Object
+     // DT of Symbol is symbol
