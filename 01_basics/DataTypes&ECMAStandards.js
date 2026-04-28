4bf3ae9dda35974f837cb01aabd6c8dc01081566
Rushikesh Barwade <rushikeshbarwade@gmail.com>
2026-04-28 18:13:37 +0000

    Added Memory Concept

--- 01_basics/DataTypes&ECMAStandards.js
+++ 01_basics/DataTypes&ECMAStandards.js
@@ -36,10 +36,53 @@ console.log(typeof undefined)
 //     number = 1224567
 // }
 
-let id = Symbol('123')
-let customerId = Symbol('123')
+// let id = Symbol('123')
+// let customerId = Symbol('123')
 
-console.log(id === customerId);
+// console.log(id === customerId);
 
 // DT of Array is Object, of Object is Object and of Function of Object Function/Function Object
      // DT of Symbol is symbol
+
+
+// ++++++++++++++ Concept of Memory++++++++++++++++++++
+
+// Stack - All primitive data types are stored in stack memory, 
+           // this memory creates a copy of it's stored value for the use by next variable
+
+// let myName = "Rushikesh"
+// let yourName = myName // Var yourName creates a copy if== of myName //copied value of original name
+
+// console.log(yourName); // O/p -> Rushikesh //prints copied name
+// console.log(myName);   // o/p-> Rushikesh //prints original name
+
+// yourName = "Vrushali" // but when same variable is given new value it stores the new value
+
+// console.log(yourName); // o/p->Vrushali //it will change copied name and not original name
+// console.log(myName);   // o/p->Rushikesh // original name stayed as is
+
+// Heap - All Non-primitive/reference DTs are stored in Heap memory, 
+          // it points to reference values i.e whatever changes happens..happens in original value itself
+
+let theName = {name: "Rushi"}
+let aName = theName
+
+console.log(aName); // o/p->Rushi
+
+aName.name = "Vrushi" // this syntax accesses the name value in object, 
+                       // this changed the original value in object i.e {name: "Rushi"}
+                         // became {name: "Vrushi"...so it will change both variables
+console.log(theName); // o/p->Vrushi
+console.log(aName); // o/p -> Vrushi
+
+
+//Summary :
+// 1. Stack keeps the original value as is throughout and just copies the original value 
+// for next variable and if next variable is changed..
+// it changes the next var only and not original var keeping it as is...
+// and it happens same for next changes made
+
+//2. Heap references toward the original value and changes the original value itself if
+//  next var is declared to change the original var returning changed value in original var.
+
+
